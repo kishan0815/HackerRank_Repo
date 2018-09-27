3ef13860a8caba0a87729fa03aee31ec5e214366
Kishan Kumar <kishan.k@techaspect.com>
2018-09-27 10:11:45 +0530

    Added source code for Array Left Rotation Challenge in Javascript

---
+++ Day_5/leftrotation.js
@@ -0,0 +1,46 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    const nd = readLine().split(' ');
+
+    const n = parseInt(nd[0], 10);
+
+    const d = parseInt(nd[1], 10);
+
+    const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+    
+
+    var temp = [n];
+    for(var j=0;j<n;j++){
+        temp[(j+n-d)%n] = a[j];
+    }
+    var result = "";
+    for(var x = 0;x<temp.length;x++)
+        result += temp[x] + ' ';
+
+    console.log(result);
+}
