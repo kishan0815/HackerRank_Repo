1deeef2420434b07977532d19d10a0dc73a01694
Kishan Kumar <kishan.k@techaspect.com>
2018-10-04 18:38:13 +0530

    Added source code for staircase challenge

---
+++ Day_10/staircase.js
@@ -0,0 +1,44 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the staircase function below.
+function staircase(n) {
+    var s = "";
+    for(var i=1;i<=n;i++) {
+        for(var j =0;j<n-i;j++) {
+            s += " ";
+        }
+        for(var k = j;k<n;k++) {
+            s += "#";
+        }
+        s += "\n";
+    }
+    console.log(s);
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    staircase(n);
+}
