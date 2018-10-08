8822bcc975b3bf7583538249a00182cb89049181
Kishan Kumar <kishan.k@tadigital.com>
2018-10-08 11:50:12 +0530

    Added source code for Counting Valleys Challenge

---
+++ Day_11/countingvalleys.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the countingValleys function below.
+function countingValleys(n, s) {
+    var vCount = 0;
+    var sum = 0;
+    for(var i=0;i<s.length;i++) {
+        if(s[i] == "D") {
+            sum -= 1;
+            if(sum < 0 && sum == -1) {
+                vCount++;
+            }
+        }
+        else {
+            sum += 1;
+        }
+    }
+    return vCount;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const s = readLine();
+
+    let result = countingValleys(n, s);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
