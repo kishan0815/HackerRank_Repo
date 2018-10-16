ddc00393b872481d6a13cbf173645f9aeb5774c7
Kishan Kumar <kishan.k@tadigital.com>
2018-10-16 10:34:13 +0530

    Added source code for sherlock and array challenge

---
+++ Day_16/sherlockandarray.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the balancedSums function below.
+function balancedSums(arr) {
+    var i = 0;
+    var j = arr.length - 1;
+    var sum = 0;
+    while (i != j) {
+        if(sum >= 0) {
+            sum -= arr[j];
+            j--;
+        } else {
+            sum +=arr[i];
+            i++;
+        }
+
+    }
+    return sum == 0 ? "YES" : "NO";
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const T = parseInt(readLine().trim(), 10);
+
+    for (let TItr = 0; TItr < T; TItr++) {
+        const n = parseInt(readLine().trim(), 10);
+
+        const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+        const result = balancedSums(arr);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
