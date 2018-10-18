a24fa4203734edb0104bd405b7ff5dbdab3bea76
Kishan Kumar <kishan.k@tadigital.com>
2018-10-18 10:32:51 +0530

    Added source code for Bigger is Greater challenge

---
+++ Day_12/biggerisgreater.js
@@ -0,0 +1,46 @@
+function processData(input) {
+    var input = input.split('\n').slice(1);
+    var i=0; 
+    for(i=0; i<input.length; i++) {
+        console.log(nextPermutation(input[i].split('')));
+    }
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
+
+function nextPermutation(array) {
+    // Find non-increasing suffix
+    var i = array.length - 1;
+    while (i > 0 && array[i - 1] >= array[i])
+        i--;
+    if (i <= 0)
+        return "no answer";
+    
+    // Find successor to pivot
+    var j = array.length - 1;
+    while (array[j] <= array[i - 1])
+        j--;
+    var temp = array[i - 1];
+    array[i - 1] = array[j];
+    array[j] = temp;
+    
+    // Reverse suffix
+    j = array.length - 1;
+    while (i < j) {
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        i++;
+        j--;
+    }
+    return array.join('');
+}
